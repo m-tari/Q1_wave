5efbf43613e096a7b1f0cdbe5634fc7eb145f513
m-tari <mohammad.tari@gmail.com>
2021-01-09 04:51:27 -0500

    removed address from env file

--- waveEnv.sh
+++ waveEnv.sh
@@ -1,5 +1,6 @@
 # add wave bin directory to path environment variable
+# go to wave code root directory and run this file
 # use: source waveEnv.sh
 
-export WAVEROOT=/home/motare/works/codes/wave_equation
+export WAVEROOT=$PWD
 export PATH=$WAVEROOT/bin:$PATH
